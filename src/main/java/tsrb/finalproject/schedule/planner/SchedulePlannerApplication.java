1cdf84d30f5d8651b5e26512ce4eab239da20448
matejkoscec <matej.koscec@gmail.com>
2022-05-07 01:45:16 +0200

    Initialize spring application

---
+++ src/main/java/tsrb/finalproject/schedule/planner/SchedulePlannerApplication.java
@@ -0,0 +1,13 @@
+package tsrb.finalproject.schedule.planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulePlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulePlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tsrb/finalproject/schedule/planner/SchedulePlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package tsrb.finalproject.schedule.planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulePlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
